a086957adbc63d0363e2d5a1efd264f41bfb47b8
Danny Makin <dpmakin@gmail.com>
2025-07-30 11:34:41 -0400

    Switch database connection from MySQL to SQLite

--- db.py
+++ db.py
@@ -2,12 +2,8 @@ import os
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "password")
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_DB = os.getenv("MYSQL_DB", "star_wars_watchlist")
-
-DATABASE_URL = f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+SQLITE_PATH = os.getenv("SQLITE_PATH", "canon_media.db")
+DATABASE_URL = f"sqlite+aiosqlite:///{SQLITE_PATH}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
